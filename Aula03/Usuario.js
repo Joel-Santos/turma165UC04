99b0b89c14db361a52e43f64b87cebd567dc2ce4
Joel Santos <joeloliveira56@gmail.com>
2025-12-17 20:03:21 -0300

    feat: Implementação da classe Usuario e exemplo de uso

--- Aula03/Usuario.js
+++ Aula03/Usuario.js
@@ -1,36 +1,109 @@
 // Definição da classe Usuario
 
-export default class Usuario{
+export default class Usuario {
     //Atributos para a classe Usuario
     //nome, idade, genero, telefone, email, cpf , cep, rg, endereco
-    nome;
-    idade;
-    genero;
-    telefone;
-    email;
-    cpf;
-    cep;
-    rg; 
-    endereco;
+    #nome;
+    #idade;
+    #genero;
+    #telefone;
+    #email;
+    #cpf;
+    #cep;
+    #rg;
+    #endereco;
 
     // Método construtor - É um método especial que é chamado/evocado quando criamos uma nova instância da classe.
     //Ele recebe os parâmetros nome, idade, genero, telefone, email, cpf , cep, rg, endereco
-    constructor(nome, idade, genero, telefone, email, cpf , cep, rg, endereco)
-    {
-        this.nome = nome;
-        this.idade = idade;
-        this.genero = genero ;
-        this.telefone = telefone;
-        this.email = email;
-        this.cpf = cpf;
-        this.cep = cep;
-        this.rg = rg;
-        this.endereco = endereco;
+    constructor(nome, idade, genero, telefone, email, cpf, cep, rg, endereco) {
+        this.#nome = nome; //setter
+        this.#idade = idade;
+        this.#genero = genero;
+        this.#telefone = telefone;
+        this.#email = email;
+        this.#cpf = cpf;
+        this.#cep = cep;
+        this.#rg = rg;
+        this.#endereco = endereco;
+
+        Object.freeze(this);
     }
 
-    exibirDados(){
+    get getNome() {
+        return this.#nome;
+    }
+    set setNome(nome) {
+        this.#nome = nome;
+    }
+
+    get getIdade() {
+        return this.#idade;
+    }
+    set setIdade(idade) {
+        this.#idade = idade;
+    }
+
+    get getGenero() {
+        return this.#genero;
+    }
+    set setGenero(genero) {
+        this.#genero = genero;
+    }
+
+    get getTelefone() {
+        return this.#telefone;
+    }
+    set setTelefone(telefone) {
+        this.#telefone = telefone;
+    }
+
+    get getEmail() {
+        return this.#email;
+    }
+    set setEmail(email) {
+        this.#email = email;
+    }
+
+    get getCpf() {
+        return this.#cpf;
+    }
+    set setCpf(cpf) {
+        this.#cpf = cpf;
+    }
+
+    get getCep() {
+        return this.#cep;
+    }
+    set setCep(cep) {
+        this.#cep = cep;
+    }
+
+    get getRg() {
+        return this.#rg;
+    }
+    set setRg(rg) {
+        this.#rg = rg;
+    }
+
+    get getEndereco() {
+        return this.#endereco;
+    }
+    set setEndereco(endereco) {
+        this.#endereco = endereco;
+    }
+
+
+    exibirDados() {
         return `
-            Nome Completo: ${this.nome}
+            Nome Completo: ${this.#nome}
+            Idade: ${this.#idade}
+            Gênero: ${this.#genero}
+            Telefone: ${this.#telefone}
+            E-mail: ${this.#email}
+            CPf : ${this.#cpf}
+            RG: ${this.#rg}
+            CEP: ${this.#cep}
+            Endereço: ${this.#endereco}
         `;
     }
 
--- Aula03/index.js
+++ Aula03/index.js
@@ -3,6 +3,11 @@ import Usuario from "./Usuario.js";
 
 //Declarando/instanciando objetos
 let usuario01 = new Usuario("Joel Santos",18, "M", "(84)4002-8922", "joel.santos@email.com", "009.900.776-90", "59298-899", "344567", "Rua dos programas n°0");
-console.log(usuario01.nome);
-usuario01.nome = "Joelson Santos";
+
+// usuario01.nome = "Joelson Santossssss";
+console.log(usuario01);
+console.log(usuario01.getNome);
+usuario01.setNome = "Jorel";
+
+console.log(usuario01.getNome);
 console.log(usuario01.exibirDados());
